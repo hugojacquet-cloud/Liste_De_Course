87cdcd60b9207896efa2533894581c420b1ac0d2
hugojacquet-cloud <hugo.jacquet2@gmail.com>
2026-07-12 15:14:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'liste-courses-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'liste-courses-v4';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './prix.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
